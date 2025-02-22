2d1fe91271ed42024327b003f39498f2be42bfd7
karlanrobles <karlanatalyrobles@gmail.com>
2025-02-21 23:05:48 -0600

    added subclass Asymmetric for asymmetric encryption to client and server, not yet tested)

--- encryption-client.py
+++ encryption-client.py
@@ -13,12 +13,39 @@ class Symmetric(Encrypt):
         self.key = key
         self.cipher_suite = Fernet(key)
 
+
     def encode(self, message):
         self.message = self.cipher_suite.encrypt(message.encode())
 
     def decode(self, data):
         self.message = self.cipher_suite.decrypt(data).decode()
 
+class Asymmetric(Encrypt):
+    def __init__(self, key):
+        self.public_key = serialization.load_pem_public_key(key)
+        
+        with open("server_private_key.pem", "rb"), f:
+            self.private_key = serialization.load_pem_private_key(f.read(), password=None)
+        
+    def encode(self, message):
+        self.message = self.public_key.encrypt(
+                message.encode(),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                    )
+                )
+
+    def decode(self, data):
+        self.message = self.private_key.decrypt(
+                data,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()), 
+                    algorithm=hashes.SHA256(),
+                    label=None
+                    )
+                )
 
 def receive_messages():
     #receive the message from the server
@@ -53,7 +80,8 @@ try:
 
     key = client.recv(1024)
 
-    encryption = Symmetric(key)
+    #encryption = Symmetric(key)
+    encryption = Asymmetric(key)
 
     receive_thread = threading.Thread(target=receive_messages)
     receive_thread.start()
--- encryption-server.py
+++ encryption-server.py
@@ -1,7 +1,8 @@
 import socket
 import threading
 from cryptography.fernet import Fernet
-
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
 
 class Encrypt:
     def __init__(self):
@@ -18,6 +19,50 @@ class Symmetric(Encrypt):
     def decode(self, data):
         self.message = self.cipher_suite.decrypt(data).decode()
 
+class Asymmetric(Encrypt):
+    def __init__(self):
+        super().__init__()
+        self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        self.public_key = self.private_key.public_key()
+        
+        self.private_bytes = self.private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PCKS58,
+                encryption_algorithm=serialization.NoEncryption())
+
+        self.public_bytes = self.public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo))
+
+        with open("server_private_key.pem", "wb") as f:
+            f.write(self.private_bytes)
+
+        with open("server_public_key.pem", "wb") as f:
+            f.write(self.public_bytes)
+
+        print("Messaging with asymmetric encryption")
+        print("RSA key pair generated successfully by server")
+
+    def encode(self, message):
+        self.message = self.public_key.encrypt(
+                message.encode(),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                    )
+                )
+
+    def decode(self, data):
+        self.message = self.private_key.decrypt(
+                data,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                    )
+                )
+
 #open socket connection through TCP
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("127.0.0.1", 12345))
@@ -30,7 +75,11 @@ def handle_client(conn, addr):
     print(f"Connection from {addr}")
 
     #send the encryption key to the client
-    conn.send(encryption.key)
+    if type(encryption) == Symmetric: 
+        conn.send(encryption.key)
+    elif type(encryption) == Asymmetric:
+        conn.send(encryption.public_bytes)
+
     while True:
         try:
             #allow user to type messages to send to the client
@@ -67,7 +116,8 @@ print("Server started. Waiting for connection...")
 server_send_thread = threading.Thread(target=send_message)
 server_send_thread.start()
 
-encryption = Symmetric()
+#encryption = Symmetric()
+encryption = Asymmetric()
 
 #close the connection and the server
 while True:
